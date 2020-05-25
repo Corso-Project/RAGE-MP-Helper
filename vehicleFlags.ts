af622a28f17437e2da63099c58016ada38e0d379
Guy Incognito <lJohhnyl@outlook.com>
2020-05-26 01:23:58 +0300

    Added vehicleFlags.ts
    https://wiki.rage.mp/index.php?title=Vehicle_Flags

---
+++ vehicleFlags.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+/**
+* Wikipedia: https://wiki.rage.mp/index.php?title=Vehicle_Flags
+*/
+enum vehicleFlags {
+    PRESSINGHORN = 1,
+    SHOOTING = 2,
+    SIRENACTIVE = 4,
+    VEHICLEDEAD = 8,
+    AIMING = 16,
+    DRIVER = 32,
+    HASAIMDATA = 64,
+    BURNOUT = 128,
+    EXITINGFVEHICLE = 256,
+    PLAYERDEAD = 512
+}
