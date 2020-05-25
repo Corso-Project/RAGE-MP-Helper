7cb7f2b876397db9fa6032b3231e245d71d74f05
Guy Incognito <lJohhnyl@outlook.com>
2020-05-26 01:21:47 +0300

    Added vehicleClasses.ts
    https://wiki.rage.mp/index.php?title=Vehicle_Classes

---
+++ vehicleClasses.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+
+/**
+* Wikipedia: https://wiki.rage.mp/index.php?title=Vehicle_Classes
+*/
+
+export enum vehicleClasses {
+    COMPACTS,
+    SEDANS,
+    SUVS,
+    COUPES,
+    MUSCLE,
+    SPORTS_CLASSIC,
+    SPORTS,
+    SUPER,
+    MOTORCYCLES,
+    OFF_ROAD,
+    INDUSTRIAl,
+    UTILITY,
+    VANS,
+    CYCLES,
+    BOATS,
+    HELICOPTERS,
+    PLANES,
+    SERVICE,
+    EMERGENCY,
+    MILITARY,
+    COMMERCIAl,
+    TRAINS
+};
