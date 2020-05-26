ffc5134b9b37e6b2f260d4762af4318b842b285e
Guy Incognito <lJohhnyl@outlook.com>
2020-05-26 17:55:38 +0300

    Added markers.ts

---
+++ markers.ts
@@ -0,0 +1,129 @@
+const Markers = {
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/60/Markers_0.png/200px-Markers_0.png)
+     */
+    PICKUP: 0,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/5/53/Markers_1.png/200px-Markers_1.png)
+     */
+    CHECKPOINT: 1,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/7/76/Markers_2.png/200px-Markers_2.png)
+     */
+    ARROW_UP: 2,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/67/Markers_3.png/200px-Markers_3.png)
+     */
+    ID: 3,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/8/84/Markers_4.png/200px-Markers_4.png)
+     */
+    FINISH: 4,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/5/50/Markers_5.png/200px-Markers_5.png)
+     */
+    FINISH_CIRCLE: 5,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/b/b6/Markers_6.png/200px-Markers_6.png)
+     */
+    CHECKPOINT_CIRCLE: 6,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/d/d1/Markers_7.png/200px-Markers_7.png)
+     */
+    PLANE: 7,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/c/c2/Markers_8.png/200px-Markers_8.png)
+     */
+    LOST_MC_TRANSPARENT: 8,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/68/Markers_9.png/200px-Markers_9.png)
+     */
+    LOST_MC: 9,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/7/70/Markers_10.png/200px-Markers_10.png)
+     */
+    ZERO: 10,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/b/b4/Markers_11.png/200px-Markers_11.png)
+     */
+    ONE: 11,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/c/c2/Markers_12.png/200px-Markers_12.png)
+     */
+    TWO: 12,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/7/7d/Markers_13.png/200px-Markers_13.png)
+     */
+    THREE: 13,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/6e/Markers_14.png/200px-Markers_14.png)
+     */
+    FOUR: 14,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/b/be/Markers_15.png/200px-Markers_15.png)
+     */
+    FIVE: 15,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/9/95/Markers_16.png/200px-Markers_16.png)
+     */
+    SIX: 16,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/b/bc/Markers_17.png/200px-Markers_17.png)
+     */
+    SEVEN: 17,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/1/14/Markers_18.png/200px-Markers_18.png)
+     */
+    EIGHT: 18,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/c/c9/Markers_19.png/200px-Markers_19.png)
+     */
+    NINE: 19,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/8/8f/Markers_20.png/200px-Markers_20.png)
+     */
+    ARROW_UP2: 20,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/9/9e/Markers_21.png/200px-Markers_21.png)
+     */
+    DOUBLE_ARROW_UP: 21,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/d/dd/Markers_22.png/200px-Markers_22.png)
+     */
+    TRIPLE_ARROW_UP: 22,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/8/8a/Markers_23.png/200px-Markers_23.png)
+     */
+    CIRCLE: 23,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/64/Markers_24.png/200px-Markers_24.png)
+     */
+    CIRCLE2: 24,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/2/27/Markers_25.png/200px-Markers_25.png)
+     */
+    CIRCLE3: 25,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/c/ca/Markers_26.png/200px-Markers_26.png)
+     */
+    CIRCLE4: 26,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/5/54/Markers_27.png/200px-Markers_27.png)
+     */
+    CIRCLE5: 27,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/0/00/Markers_28.png/200px-Markers_28.png)
+     */
+    SPHERE: 28,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/d/d2/Markers_29.png/200px-Markers_29.png)
+     */
+    MONEY: 29,
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/6/64/Markers_30.png/200px-Markers_30.png)
+     */
+    ANIMAL: 30
+    /**
+     * ![](https://wiki.rage.mp//images/thumb/d/d0/Markers_31.png/200px-Markers_31.png)
+     */
+};
